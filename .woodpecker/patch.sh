7e81bdada30a061ba380c53d2d7703276ce83b7f
Chimmie Firefly <gameplayer2019pl@tutamail.com>
2025-09-16 18:05:52 +0100

    Remove useless newlines.

--- .woodpecker/patch.sh
+++ .woodpecker/patch.sh
@@ -2,9 +2,9 @@
 
 # Workaround for shared certificates for forgejo.xvm domain
 if [ "${1:-forgejo.xvm}" = "forgejo.xvm" ]; then
-	mkdir -p /var/lib/dpkg/info/;
-	touch /var/lib/dpkg/info/ca-certificates.list;
-	touch /var/lib/dpkg/info/ca-certificates-java.list;
-	dpkg --set-selections <<< "ca-certificates install";
-	dpkg --set-selections <<< "ca-certificates-java install";
+	mkdir -p /var/lib/dpkg/info/
+	touch /var/lib/dpkg/info/ca-certificates.list
+	touch /var/lib/dpkg/info/ca-certificates-java.list
+	dpkg --set-selections <<< "ca-certificates install"
+	dpkg --set-selections <<< "ca-certificates-java install"
 fi
