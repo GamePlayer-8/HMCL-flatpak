6fc6684ab611a7712530e5e5870d4debf11e8f15
Chimmie Firefly <gameplayer2019pl@tutamail.com>
2025-09-16 18:14:41 +0100

    test 3

--- .woodpecker/patch.sh
+++ .woodpecker/patch.sh
@@ -4,7 +4,7 @@
 if [ "${1:-forgejo.xvm}" = "forgejo.xvm" ]; then
 	mkdir -p /var/lib/dpkg/info/
 	touch /var/lib/dpkg/info/ca-certificates.list
-	touch /var/lib/dpkg/info/ca-certificates-java.list
+#	touch /var/lib/dpkg/info/ca-certificates-java.list
 	echo "ca-certificates install" | dpkg --set-selections
-	echo "ca-certificates-java install" | dpkg --set-selections
+#	echo "ca-certificates-java install" | dpkg --set-selections
 fi
