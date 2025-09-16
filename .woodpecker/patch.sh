d73188db2a5fb379cd618012fa8067c33b24b219
Chimmie Firefly <gameplayer2019pl@tutamail.com>
2025-09-16 18:05:26 +0100

    Implement as patch.

---
+++ .woodpecker/patch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Workaround for shared certificates for forgejo.xvm domain
+if [ "${1:-forgejo.xvm}" = "forgejo.xvm" ]; then
+	mkdir -p /var/lib/dpkg/info/;
+	touch /var/lib/dpkg/info/ca-certificates.list;
+	touch /var/lib/dpkg/info/ca-certificates-java.list;
+	dpkg --set-selections <<< "ca-certificates install";
+	dpkg --set-selections <<< "ca-certificates-java install";
+fi
